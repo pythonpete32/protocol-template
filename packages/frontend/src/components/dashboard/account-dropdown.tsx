a4c245b32c4b754aa263344eb3ae6f135ce2dd31
pythonpete32 <aaron@DAOresear.ch>
2024-04-04 10:59:08 +0400

    refactor dashboard

---
+++ packages/frontend/src/components/dashboard/account-dropdown.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { CircleUser } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function AccountDropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon" className="rounded-full">
+          <CircleUser className="h-4 w-4" />
+          <span className="sr-only">Toggle account menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Account</DropdownMenuLabel>
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
