1687f515ecd4ec926549da19f128aca731ab7dc2
pythonpete32 <aaron@DAOresear.ch>
2024-04-16 20:25:01 +0400

    Remove unused BalanceCardServer component

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -55,26 +55,3 @@ const BalanceCard: React.FC = () => {
     </Card>
   );
 };
-
-const BalanceCardServer: React.FC = async () => {
-  let blockNumber;
-
-  await watchBlocks(config, {
-    onBlock(block) {
-      console.log("Block changed!", block);
-      blockNumber = block.number;
-    },
-  });
-
-  return (
-    <Card>
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">Account Balance</CardTitle>
-        <DollarSign className="h-4 w-4 text-muted-foreground" />
-      </CardHeader>
-      <CardContent>
-        <div className="text-2xl font-bold">{blockNumber}</div>
-      </CardContent>
-    </Card>
-  );
-};
